891332da2110e921863a8dd4760346ec619a9e16
DanTheMan52 <37552203+DanTheMan52@users.noreply.github.com>
2024-07-06 08:48:58 +0200

    Add files via upload
    shader fix

--- shaders/tracer_vertex.glsl
+++ shaders/tracer_vertex.glsl
@@ -1,10 +1,12 @@
 #version 430 core
 layout(location = 0) in vec3 aPos;
+out vec3 FragPos;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
 void main() {
+    FragPos = vec3(model * vec4(aPos, 1.0));
     gl_Position = projection * view * model * vec4(aPos, 1.0);
 }
